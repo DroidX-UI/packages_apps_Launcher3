SuperiorOS/android_packages_apps_Launcher3

[SQUASH]Launcher3: Bring back QuickSpace events
* This is wholesome squash of work done in 11.0 of crDroid Home
* History commits - https://github.com/crdroidandroid/android_packages_apps_Launcher3/commits/11.0/src/com/android/launcher3/quickspace

Launcher3: Make weather condition option

QuickSpace: Apply at a glance widget design

Launcher3: Rework at a glance view for QPR1

big thanks and credits to neobuddy89 for fixing and forward port of quickspace

* refactor quickevents
  - fixed random messages reverting back to singeline on certain time/hours, leaving a broken weather layout
  - update quick event strings
  - use static info icon instead of icons which were similar to weather icons
  - remove singleline transition, hide quickevent messages views instead if disabled so that now playing can access the views for doubleline now playing layout.

* refactor now playing layout
  - update player icon and follow lawnchair now playing layout (without the viewpager)
  - show unknown artist if artist name is null/unretrievable
  - automatically hide weather when playback is active

* Minimalistic style -> Extended at a glance
  - minimalist but extended style of google's at a glance first inspired by pixel launchermod by teamfiles, revised over time and releases

* refactor weather layout
  - add a filtered/revised weather condition

* polish at a glance layout issues
  - fixed clipped shadow due to wrong usage of margins on initial at a glance layout change
  - misc layout fixing from initial at a glance layout changes

quickspace: layout tweaks

QuickSpace: Improve Now playing for extended style

QuickSpace: Implement onclick redirects

QuickSpace: Detailed weather toggle

Quickspace: Optional weather location

QuickSpace: Fix weather city spacing

Co-authored-by: Chris <chris@cypheros.co>
Co-authored-by: El Dainosor <eldainosor@gmail.com>
Co-authored-by: Ali B <abittin@gmail.com>
Co-authored-by: Till Kottmann <me@deletescape.ch>
Co-authored-by: minaripenguin <mina

src/com/android/launcher3/quickspace/QuickspaceController.java
@@ -1,0 +1,286 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.quickspace;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.RemoteControlClient;
+import android.media.RemoteController;
+import android.os.Handler;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.android.internal.util.superior.OmniJawsClient;
+
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.notification.NotificationKeyData;
+import com.android.launcher3.notification.NotificationListener;
+import com.android.launcher3.util.PackageUserKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickspaceController implements NotificationListener.NotificationsChangedListener, OmniJawsClient.OmniJawsObserver {
+
+    public final ArrayList<OnDataListener> mListeners = new ArrayList();
+    private static final String SETTING_WEATHER_LOCKSCREEN_UNIT = "weather_lockscreen_unit";
+    private static final boolean DEBUG = false;
+    private static final String TAG = "Launcher3:QuickspaceController";
+
+    private final Context mContext;
+    private final Handler mHandler;
+    private QuickEventsController mEventsController;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherInfo;
+    private Drawable mConditionImage;
+
+    private boolean mUseImperialUnit;
+
+    private AudioManager mAudioManager;
+    private Metadata mMetadata = new Metadata();
+    private RemoteController mRemoteController;
+    private boolean mClientLost = true;
+    private boolean mMediaActive = false;
+
+    public interface OnDataListener {
+        void onDataUpdated();
+    }
+
+    public QuickspaceController(Context context) {
+        mContext = context;
+        mHandler = new Handler();
+        mEventsController = new QuickEventsController(context);
+        mWeatherClient = new OmniJawsClient(context);
+        mRemoteController = new RemoteController(context, mRCClientUpdateListener);
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.registerRemoteController(mRemoteController);
+    }
+
+    private void addWeatherProvider() {
+        if (!Utilities.isQuickspaceWeather(mContext)) return;
+        mWeatherClient.addObserver(this);
+        queryAndUpdateWeather();
+    }
+
+    public void addListener(OnDataListener listener) {
+        mListeners.add(listener);
+        addWeatherProvider();
+        listener.onDataUpdated();
+    }
+
+    public void removeListener(OnDataListener listener) {
+        if (mWeatherClient != null) {
+            mWeatherClient.removeObserver(this);
+        }
+        mListeners.remove(listener);
+    }
+
+    public boolean isQuickEvent() {
+        return mEventsController.isQuickEvent();
+    }
+
+    public QuickEventsController getEventController() {
+        return mEventsController;
+    }
+
+    public boolean isWeatherAvailable() {
+        return mWeatherClient != null && mWeatherClient.isOmniJawsEnabled();
+    }
+
+    public Drawable getWeatherIcon() {
+        return mConditionImage;
+    }
+
+    public String getWeatherTemp() {
+    	boolean isDetailed = Utilities.isQuickSpaceWeatherDetailed(mContext);
+    	boolean shouldShowCity = Utilities.QuickSpaceShowCity(mContext);
+        if (mWeatherInfo != null) {
+            String formattedCondition = mWeatherInfo.condition;
+            if (formattedCondition.toLowerCase().contains("clouds")) {
+               formattedCondition = "Cloudy";
+            } else if (formattedCondition.toLowerCase().contains("rain")) {
+              formattedCondition = "Rainy";
+            } else if (formattedCondition.toLowerCase().contains("clear")) {
+              formattedCondition = "Sunny";
+            } else if (formattedCondition.toLowerCase().contains("storm")) {
+              formattedCondition = "Stormy";
+            } else if (formattedCondition.toLowerCase().contains("snow")) {
+              formattedCondition = "Snowy";
+            } else if (formattedCondition.toLowerCase().contains("wind")) {
+              formattedCondition = "Windy";
+            } else if (formattedCondition.toLowerCase().contains("mist")) {
+              formattedCondition = "Misty";
+            }
+            String weatherTemp = (shouldShowCity ? mWeatherInfo.city + " " : "") + mWeatherInfo.temp + mWeatherInfo.tempUnits + (isDetailed ? " - "  + formattedCondition : "");
+            return weatherTemp;
+        }
+        return null;
+    }
+
+    private void playbackStateUpdate(int state) {
+        boolean active;
+        switch (state) {
+            case RemoteControlClient.PLAYSTATE_PLAYING:
+                active = true;
+                break;
+            case RemoteControlClient.PLAYSTATE_ERROR:
+            case RemoteControlClient.PLAYSTATE_PAUSED:
+            default:
+                active = false;
+                break;
+        }
+        if (active != mMediaActive) {
+            mMediaActive = active;
+        }
+        updateMediaInfo();
+    }
+
+    public void updateMediaInfo() {
+        if (mEventsController != null) {
+            mEventsController.setMediaInfo(mMetadata.trackTitle, mMetadata.trackArtist, mClientLost, mMediaActive);
+            mEventsController.updateQuickEvents();
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void onNotificationPosted(PackageUserKey postedPackageUserKey,
+                     NotificationKeyData notificationKey) {
+        updateMediaInfo();
+    }
+
+    @Override
+    public void onNotificationRemoved(PackageUserKey removedPackageUserKey,
+                      NotificationKeyData notificationKey) {
+        updateMediaInfo();
+    }
+
+    @Override
+    public void onNotificationFullRefresh(List<StatusBarNotification> activeNotifications) {
+        updateMediaInfo();
+    }
+
+    public void onPause() {
+        if (mEventsController != null) mEventsController.onPause();
+    }
+
+    public void onResume() {
+        if (mEventsController != null) {
+            updateMediaInfo();
+            mEventsController.onResume();
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        Log.d(TAG, "weatherError " + errorReason);
+        if (errorReason == OmniJawsClient.EXTRA_ERROR_DISABLED) {
+            mWeatherInfo = null;
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void updateSettings() {
+        Log.i(TAG, "updateSettings");
+        queryAndUpdateWeather();
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            mWeatherClient.queryWeather();
+            mWeatherInfo = mWeatherClient.getWeatherInfo();
+            if (mWeatherInfo != null) {
+                mConditionImage = mWeatherClient.getWeatherConditionImage(mWeatherInfo.conditionCode);
+            }
+            notifyListeners();
+        } catch(Exception e) {
+            // Do nothing
+        }
+    }
+
+    public void notifyListeners() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (OnDataListener list : mListeners) {
+                    list.onDataUpdated();
+                }
+            }
+        });
+    }
+
+   private RemoteController.OnClientUpdateListener mRCClientUpdateListener =
+            new RemoteController.OnClientUpdateListener() {
+
+        @Override
+        public void onClientChange(boolean clearing) {
+            if (clearing) {
+                mMetadata.clear();
+                mMediaActive = false;
+                mClientLost = true;
+            }
+            updateMediaInfo();
+        }
+
+        @Override
+        public void onClientPlaybackStateUpdate(int state, long stateChangeTimeMs,
+                long currentPosMs, float speed) {
+            mClientLost = false;
+            playbackStateUpdate(state);
+        }
+
+        @Override
+        public void onClientPlaybackStateUpdate(int state) {
+            mClientLost = false;
+            playbackStateUpdate(state);
+        }
+
+        @Override
+        public void onClientMetadataUpdate(RemoteController.MetadataEditor data) {
+            mMetadata.trackTitle = data.getString(MediaMetadataRetriever.METADATA_KEY_TITLE,
+                    mMetadata.trackTitle);
+            mMetadata.trackArtist = data.getString(MediaMetadataRetriever.METADATA_KEY_ARTIST,
+                    mMetadata.trackArtist);
+            mClientLost = false;
+            updateMediaInfo();
+        }
+
+        @Override
+        public void onClientTransportControlUpdate(int transportControlFlags) {
+        }
+    };
+
+    class Metadata {
+        private String trackTitle;
+        private String trackArtist;
+
+         public void clear() {
+            trackTitle = null;
+            trackArtist = null;
+        }
+    }
+}

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -50,6 +50,8 @@ import com.android.launcher3.model.WidgetsModel;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
+import com.android.internal.util.superior.OmniJawsClient;
+
 /**
  * Settings activity for Launcher.
  */
@@ -108,6 +110,13 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_HOTSEAT_OPACITY:
             case Utilities.KEY_SHORT_PARALLAX:
             case Utilities.KEY_SINGLE_PAGE_CENTER:
+            case Utilities.DESKTOP_SHOW_QUICKSPACE:
+            case Utilities.KEY_SHOW_ALT_QUICKSPACE:
+            case Utilities.KEY_SHOW_QUICKSPACE_NOWPLAYING:
+            case Utilities.KEY_SHOW_QUICKSPACE_WEATHER:
+            case Utilities.KEY_SHOW_QUICKSPACE_DETAILED_WEATHER: 
+            case Utilities.KEY_SHOW_QUICKSPACE_PSONALITY:
+            case Utilities.KEY_QUICKSPACE_SHOW_CITY:
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
                 break;
             default:
@@ -168,6 +177,9 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
 
         private Preference mShowGoogleAppPref;
         private Preference mShowGoogleBarPref;
+        private Preference mWeatherPref;
+
+        private OmniJawsClient mWeatherClient;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -195,6 +207,13 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             mShowGoogleAppPref = screen.findPreference(KEY_MINUS_ONE);
             mShowGoogleBarPref = screen.findPreference(Utilities.KEY_DOCK_SEARCH);
             updateIsGoogleAppEnabled();
+
+            mWeatherClient = new OmniJawsClient(getContext());
+            mWeatherPref = screen.findPreference(Utilities.KEY_SHOW_QUICKSPACE_WEATHER);
+            if (!mWeatherClient.isOmniJawsEnabled()) {
+                mWeatherPref.setEnabled(false);
+                mWeatherPref.setSummary(R.string.quick_event_ambient_weather_enabled_info);
+            }
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 if (getPreferenceScreen().getTitle().equals(

DroidX-UI/packages_apps_Launcher3

Co-Authored-By: Chris <chris@cypheros.co>
Co-Authored-By: El Dainosor <eldainosor@gmail.com>
Co-Authored-By: Ali B <abittin@gmail.com>
Co-Authored-By: Till Kottmann <me@deletescape.ch>
Co-Authored-By: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/launcher3/quickspace/QuickspaceController.java
@@ -1,0 +1,286 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.quickspace;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.RemoteControlClient;
+import android.media.RemoteController;
+import android.os.Handler;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.android.internal.util.droidx.OmniJawsClient;
+
+import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.notification.NotificationKeyData;
+import com.android.launcher3.notification.NotificationListener;
+import com.android.launcher3.util.PackageUserKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickspaceController implements NotificationListener.NotificationsChangedListener, OmniJawsClient.OmniJawsObserver {
+
+    public final ArrayList<OnDataListener> mListeners = new ArrayList();
+    private static final String SETTING_WEATHER_LOCKSCREEN_UNIT = "weather_lockscreen_unit";
+    private static final boolean DEBUG = false;
+    private static final String TAG = "Launcher3:QuickspaceController";
+
+    private final Context mContext;
+    private final Handler mHandler;
+    private QuickEventsController mEventsController;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherInfo;
+    private Drawable mConditionImage;
+
+    private boolean mUseImperialUnit;
+
+    private AudioManager mAudioManager;
+    private Metadata mMetadata = new Metadata();
+    private RemoteController mRemoteController;
+    private boolean mClientLost = true;
+    private boolean mMediaActive = false;
+
+    public interface OnDataListener {
+        void onDataUpdated();
+    }
+
+    public QuickspaceController(Context context) {
+        mContext = context;
+        mHandler = new Handler();
+        mEventsController = new QuickEventsController(context);
+        mWeatherClient = new OmniJawsClient(context);
+        mRemoteController = new RemoteController(context, mRCClientUpdateListener);
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.registerRemoteController(mRemoteController);
+    }
+
+    private void addWeatherProvider() {
+        if (!Utilities.isQuickspaceWeather(mContext)) return;
+        mWeatherClient.addObserver(this);
+        queryAndUpdateWeather();
+    }
+
+    public void addListener(OnDataListener listener) {
+        mListeners.add(listener);
+        addWeatherProvider();
+        listener.onDataUpdated();
+    }
+
+    public void removeListener(OnDataListener listener) {
+        if (mWeatherClient != null) {
+            mWeatherClient.removeObserver(this);
+        }
+        mListeners.remove(listener);
+    }
+
+    public boolean isQuickEvent() {
+        return mEventsController.isQuickEvent();
+    }
+
+    public QuickEventsController getEventController() {
+        return mEventsController;
+    }
+
+    public boolean isWeatherAvailable() {
+        return mWeatherClient != null && mWeatherClient.isOmniJawsEnabled();
+    }
+
+    public Drawable getWeatherIcon() {
+        return mConditionImage;
+    }
+
+    public String getWeatherTemp() {
+    	boolean isDetailed = Utilities.isQuickSpaceWeatherDetailed(mContext);
+    	boolean shouldShowCity = Utilities.QuickSpaceShowCity(mContext);
+        if (mWeatherInfo != null) {
+            String formattedCondition = mWeatherInfo.condition;
+            if (formattedCondition.toLowerCase().contains("clouds")) {
+               formattedCondition = "Cloudy";
+            } else if (formattedCondition.toLowerCase().contains("rain")) {
+              formattedCondition = "Rainy";
+            } else if (formattedCondition.toLowerCase().contains("clear")) {
+              formattedCondition = "Sunny";
+            } else if (formattedCondition.toLowerCase().contains("storm")) {
+              formattedCondition = "Stormy";
+            } else if (formattedCondition.toLowerCase().contains("snow")) {
+              formattedCondition = "Snowy";
+            } else if (formattedCondition.toLowerCase().contains("wind")) {
+              formattedCondition = "Windy";
+            } else if (formattedCondition.toLowerCase().contains("mist")) {
+              formattedCondition = "Misty";
+            }
+            String weatherTemp = (shouldShowCity ? mWeatherInfo.city + " " : "") + mWeatherInfo.temp + mWeatherInfo.tempUnits + (isDetailed ? " - "  + formattedCondition : "");
+            return weatherTemp;
+        }
+        return null;
+    }
+
+    private void playbackStateUpdate(int state) {
+        boolean active;
+        switch (state) {
+            case RemoteControlClient.PLAYSTATE_PLAYING:
+                active = true;
+                break;
+            case RemoteControlClient.PLAYSTATE_ERROR:
+            case RemoteControlClient.PLAYSTATE_PAUSED:
+            default:
+                active = false;
+                break;
+        }
+        if (active != mMediaActive) {
+            mMediaActive = active;
+        }
+        updateMediaInfo();
+    }
+
+    public void updateMediaInfo() {
+        if (mEventsController != null) {
+            mEventsController.setMediaInfo(mMetadata.trackTitle, mMetadata.trackArtist, mClientLost, mMediaActive);
+            mEventsController.updateQuickEvents();
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void onNotificationPosted(PackageUserKey postedPackageUserKey,
+                     NotificationKeyData notificationKey) {
+        updateMediaInfo();
+    }
+
+    @Override
+    public void onNotificationRemoved(PackageUserKey removedPackageUserKey,
+                      NotificationKeyData notificationKey) {
+        updateMediaInfo();
+    }
+
+    @Override
+    public void onNotificationFullRefresh(List<StatusBarNotification> activeNotifications) {
+        updateMediaInfo();
+    }
+
+    public void onPause() {
+        if (mEventsController != null) mEventsController.onPause();
+    }
+
+    public void onResume() {
+        if (mEventsController != null) {
+            updateMediaInfo();
+            mEventsController.onResume();
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        Log.d(TAG, "weatherError " + errorReason);
+        if (errorReason == OmniJawsClient.EXTRA_ERROR_DISABLED) {
+            mWeatherInfo = null;
+            notifyListeners();
+        }
+    }
+
+    @Override
+    public void updateSettings() {
+        Log.i(TAG, "updateSettings");
+        queryAndUpdateWeather();
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            mWeatherClient.queryWeather();
+            mWeatherInfo = mWeatherClient.getWeatherInfo();
+            if (mWeatherInfo != null) {
+                mConditionImage = mWeatherClient.getWeatherConditionImage(mWeatherInfo.conditionCode);
+            }
+            notifyListeners();
+        } catch(Exception e) {
+            // Do nothing
+        }
+    }
+
+    public void notifyListeners() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (OnDataListener list : mListeners) {
+                    list.onDataUpdated();
+                }
+            }
+        });
+    }
+
+   private RemoteController.OnClientUpdateListener mRCClientUpdateListener =
+            new RemoteController.OnClientUpdateListener() {
+
+        @Override
+        public void onClientChange(boolean clearing) {
+            if (clearing) {
+                mMetadata.clear();
+                mMediaActive = false;
+                mClientLost = true;
+            }
+            updateMediaInfo();
+        }
+
+        @Override
+        public void onClientPlaybackStateUpdate(int state, long stateChangeTimeMs,
+                long currentPosMs, float speed) {
+            mClientLost = false;
+            playbackStateUpdate(state);
+        }
+
+        @Override
+        public void onClientPlaybackStateUpdate(int state) {
+            mClientLost = false;
+            playbackStateUpdate(state);
+        }
+
+        @Override
+        public void onClientMetadataUpdate(RemoteController.MetadataEditor data) {
+            mMetadata.trackTitle = data.getString(MediaMetadataRetriever.METADATA_KEY_TITLE,
+                    mMetadata.trackTitle);
+            mMetadata.trackArtist = data.getString(MediaMetadataRetriever.METADATA_KEY_ARTIST,
+                    mMetadata.trackArtist);
+            mClientLost = false;
+            updateMediaInfo();
+        }
+
+        @Override
+        public void onClientTransportControlUpdate(int transportControlFlags) {
+        }
+    };
+
+    class Metadata {
+        private String trackTitle;
+        private String trackArtist;
+
+         public void clear() {
+            trackTitle = null;
+            trackArtist = null;
+        }
+    }
+}

src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -50,6 +50,8 @@ import com.android.launcher3.model.WidgetsModel;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
+import com.android.internal.util.droidx.OmniJawsClient;
+
 /**
  * Settings activity for Launcher.
  */
@@ -108,6 +110,13 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_HOTSEAT_OPACITY:
             case Utilities.KEY_SHORT_PARALLAX:
             case Utilities.KEY_SINGLE_PAGE_CENTER:
+            case Utilities.DESKTOP_SHOW_QUICKSPACE:
+            case Utilities.KEY_SHOW_ALT_QUICKSPACE:
+            case Utilities.KEY_SHOW_QUICKSPACE_NOWPLAYING:
+            case Utilities.KEY_SHOW_QUICKSPACE_WEATHER:
+            case Utilities.KEY_SHOW_QUICKSPACE_DETAILED_WEATHER: 
+            case Utilities.KEY_SHOW_QUICKSPACE_PSONALITY:
+            case Utilities.KEY_QUICKSPACE_SHOW_CITY:
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
                 break;
             default:
@@ -168,6 +177,9 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
 
         private Preference mShowGoogleAppPref;
         private Preference mShowGoogleBarPref;
+        private Preference mWeatherPref;
+
+        private OmniJawsClient mWeatherClient;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -195,6 +207,13 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             mShowGoogleAppPref = screen.findPreference(KEY_MINUS_ONE);
             mShowGoogleBarPref = screen.findPreference(Utilities.KEY_DOCK_SEARCH);
             updateIsGoogleAppEnabled();
+
+            mWeatherClient = new OmniJawsClient(getContext());
+            mWeatherPref = screen.findPreference(Utilities.KEY_SHOW_QUICKSPACE_WEATHER);
+            if (!mWeatherClient.isOmniJawsEnabled()) {
+                mWeatherPref.setEnabled(false);
+                mWeatherPref.setSummary(R.string.quick_event_ambient_weather_enabled_info);
+            }
 
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 if (getPreferenceScreen().getTitle().equals(