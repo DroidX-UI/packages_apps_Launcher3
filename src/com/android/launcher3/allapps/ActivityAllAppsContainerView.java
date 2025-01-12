ae78e45801b16d9beec5bc01d40c67df89687cbf
=?UTF-8?q?Ph=E1=BA=A1m=20Ti=C3=AAn=20Sinh?= <song.long.dai.duong@gmail.com>
2025-01-12 14:58:14 +0100

    fixup! DroidX-UI Launcher: Refine dock and all apps UI
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -306,7 +306,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
         super.onFinishInflate();
         if (Utilities.showSearch(getContext())) {
             mSearchContainer.setVisibility(View.VISIBLE);
-            if (Themes.isThemedIconEnabled(mContext)) {
+            if (!Themes.isThemedIconEnabled(mContext)) {
                 getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google);
             } else {
                 getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
@@ -811,7 +811,7 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
             mTabsProtectionAlpha = tabsAlpha;
             invalidateHeader();
         }
-        if (Themes.isThemedIconEnabled(mContext)) {
+        if (!Themes.isThemedIconEnabled(mContext)) {
             getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google);
         } else {
             getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
